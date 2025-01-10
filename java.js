160a8fe7257d409c08fd223a8cdfc999b46d9eb7
sila <santiagojosesilverio@gmail.com>
2025-01-09 23:13:37 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.navbar2 a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 70,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
